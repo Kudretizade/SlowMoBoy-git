9a75b84bde93745b2bbcb16e2650c8c752b3103b
Kudretizade <104269357+Kudretizade@users.noreply.github.com>
2022-08-26 22:33:05 +0400

    UI Improved, 1. level uncompleted
    I have to find character and find animations (idle, forward)

--- SlowMoBoy/Assets/App/Scripts/UIController.cs
+++ SlowMoBoy/Assets/App/Scripts/UIController.cs
@@ -9,23 +9,52 @@ public class UIController : MonoBehaviour
 
     private int MainMenuScene = 0;
 
+    private int OptionsScene = 2;
+
+    private int LevelSelectScene = 3;
 
 
-    public void CreditsButton()
-    {
-        SceneManager.LoadScene(CreditsScene);
-    }
+    //level 1 = 4 id
+
+    private int Level1 = 4;
+    private int Level2 = 5;
+    private int Level3 = 6;
+
+//----------------------------------------------------
 
     public void MainMenuButton()
     {
         SceneManager.LoadScene(MainMenuScene);
     }
-
+    public void LevelSelectButton()
+    {
+        SceneManager.LoadScene(LevelSelectScene);
+    }
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene(OptionsScene);
+    }
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene(CreditsScene);
+    }
     public void QuitGameButton()
     {
         Application.Quit();
         Debug.Log("Application closed");
     }
+//----------------------------------------------------
 
-
+    public void Level1Button()
+    {
+        SceneManager.LoadScene(Level1);
+    }
+    public void Level2Button()
+    {
+        SceneManager.LoadScene(Level2);
+    }
+    public void Level3Button()
+    {
+        SceneManager.LoadScene(Level3);
+    }
 }
